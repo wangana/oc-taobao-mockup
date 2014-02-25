4d7fa8feb5a0cdf50948907516a66c35431fa38e
unknown <Administrator@Ana-PC.(none)>
2014-02-25 11:09:59 +0800

    img file

---
+++ js/index.js
@@ -0,0 +1,39 @@
+ $(function(){
+    $('.dropdown').hover(
+      function(){
+        $(this).addClass('open');
+      },
+      function(){
+        $(this).removeClass('open');
+      }
+    );
+  });
+
+
+  $(function(){
+    $('.dropdown favorite').hover(
+      function(){
+        $(this).addClass('open');
+      },
+      function(){
+        $(this).removeClass('open');
+      }
+    );
+  });
+
+$(document).ready(function()
+{
+	$(".but-code").mouseover(function(){
+		$(".but-code").css("background-color","#ff4500");
+	});
+	$(".but-code").mouseout(function(){
+		$(".but-code").css("background-color","#ffffff");
+	});
+
+	$("button").click(function()
+	{
+		$(".Qr-code").hide();
+	});
+
+});
+
