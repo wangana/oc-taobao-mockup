1036cb6efe5c34fde70390321023e29c8213803a
Joa <joa@originatechina.com>
2014-02-27 18:10:12 +0800

    search

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
 
 
- $(function(){
-    $('.dropdown').hover(
-      function(){
-        $(this).addClass('open');
-      },
-      function(){
-        $(this).removeClass('open');
-      }
+$(function(){
+  $('.dropdown').hover(
+    function(){
+      $(this).addClass('open');
+    },
+    function(){
+      $(this).removeClass('open');
+    }
     );
-  });
+});
 
 
-  $(function(){
-    $('.dropdown favorite').hover(
-      function(){
-        $(this).addClass('open');
-      },
-      function(){
-        $(this).removeClass('open');
-      }
+$(function(){
+  $('.dropdown favorite').hover(
+    function(){
+      $(this).addClass('open');
+    },
+    function(){
+      $(this).removeClass('open');
+    }
     );
-  });
+});
 
 $(document).ready(function()
 {
@@ -32,10 +32,35 @@ $(document).ready(function()
 		$(".but-code").css("background-color","#ffffff");
 	});
 
-	$("button").click(function()
+	$(".but-code").click(function()
 	{
 		$(".Qr-code").hide();
 	});
+
+
+  $("#searchtab").mouseover(function(){
+    $("#searchtab").css("background-color"," #f5f5f5");
+    $("#searchtab-h").css("background-color"," #ffffff");
+  });
+  $("#searchtab").mouseout(function(){
+    $("#searchtab").css("background-color","#ffffff");
+    $("#searchtab-h").css("background-color","#f5f5f5");
+  });
+  $("#searchtab").click(function(){
+    if ($(this).hasClass("shop")){
+      $("#searchtab a").html('<a href="#">宝贝</a>');
+      $("#searchtab-h a").html('<a href="#">店铺<b class="caret"></b></a>');
+      $(this).removeClass("shop").addClass("item");
+    }else{
+      $("#searchtab a").html('<a href="#">店铺</a>');
+      $("#searchtab-h a").html('<a href="#">宝贝<b class="caret"></b></a>');
+      $(this).removeClass("item").addClass("shop");
+    }
+  });
+
+  $("#input").click(function(){
+    $(".iconfont").hide();
+  });
 
 });
 
