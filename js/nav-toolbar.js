525d8695600b544fe25af3b1ab530307f02c46d6
Joa <joa@originatechina.com>
2014-02-28 11:08:18 +0800

    Code layout

---
+++ js/nav-toolbar.js
@@ -0,0 +1,21 @@
+$(function(){
+  $('.dropdown').hover(
+    function(){
+      $(this).addClass('open');
+    },
+    function(){
+      $(this).removeClass('open');
+    }
+    );
+});
+
+$(function(){
+  $('.dropdown favorite').hover(
+    function(){
+      $(this).addClass('open');
+    },
+    function(){
+      $(this).removeClass('open');
+    }
+    );
+});
--- js/search-box.js
+++ js/search-box.js
@@ -1,28 +1,3 @@
-
-
-$(function(){
-  $('.dropdown').hover(
-    function(){
-      $(this).addClass('open');
-    },
-    function(){
-      $(this).removeClass('open');
-    }
-    );
-});
-
-
-$(function(){
-  $('.dropdown favorite').hover(
-    function(){
-      $(this).addClass('open');
-    },
-    function(){
-      $(this).removeClass('open');
-    }
-    );
-});
-
 $(document).ready(function()
 {
 	$(".but-code").mouseover(function(){
