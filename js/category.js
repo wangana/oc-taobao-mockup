f5c05d11fa37382e4dbe8c82def4b9b100d87002
Joa <joa@originatechina.com>
2014-03-28 16:23:19 +0800

    Arrange

---
+++ js/category.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+	// $(".category-info").mouseover(function(){
+ //    $(".category-info").css({"width":"150px","z-index":"10","border-top":"1px solid","border-left":"1px solid","border-bottom":"1px solid","border-color":"#f40","border-right":"2px solid #fff"});
+ //    $(".menu-item").css({"border":"1px solid #f40","z-index":"3","display":"block"});
+ //  });
+})
+
+
+
+
+
+
+
