8379ec4a5214b8d223d2a943920a0513c4b1f44f
Joa <joa@originatechina.com>
2014-03-31 16:14:12 +0800

    Supplement : category-list

--- js/category.js
+++ js/category.js
@@ -1,14 +1,38 @@
-$(document).ready(function()
-{
-	// $(".category-info").mouseover(function(){
- //    $(".category-info").css({"width":"150px","z-index":"10","border-top":"1px solid","border-left":"1px solid","border-bottom":"1px solid","border-color":"#f40","border-right":"2px solid #fff"});
- //    $(".menu-item").css({"border":"1px solid #f40","z-index":"3","display":"block"});
- //  });
-})
+var delay = 200;
+var allCateTimer = null;
 
-
-
-
-
-
-
+$('div#nav > ul > li').hover(function(ev){
+	
+	var $this = this;
+	
+	allCateTimer = setTimeout(function(){
+		var bottomHeight = document.documentElement.clientHeight - ev.clientY;
+		if(bottomHeight <= 250){
+			$($this).addClass('over').find('div.submenubox').addClass('submenuboxBottom').removeClass('disn');
+		}else{
+			$($this).addClass('over').find('div.submenubox').removeClass('submenuboxBottom').removeClass('disn');
+		}
+		
+		//兼容IE6隐藏所有select 元素
+		$("select").each(function(){
+			if($(this).css('visibility') != 'hidden'){
+				$(this).addClass('menuVisible').css('visibility', 'hidden');
+			}
+		});
+		
+		if($($this).find("div.subcate").size()<= 0){
+			async_topMenu($this);
+		}
+	}, delay);
+	
+},function(){
+	
+	var $this = this;
+	if(allCateTimer){
+		clearTimeout(allCateTimer);
+	}
+	allCateTimer = setTimeout(function(){ 
+		$($this).removeClass('over').find('div.submenubox').addClass('disn');}, delay);
+	}
+	
+);
