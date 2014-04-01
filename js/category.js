ced69ef421c56c8ec1de45389333863539cced41
Joa <joa@originatechina.com>
2014-04-01 14:03:31 +0800

    modfied

--- js/category.js
+++ js/category.js
@@ -3,14 +3,14 @@ var allCateTimer = null;
 
 $('div#nav > ul > li').hover(function(ev){
 	
-	var $this = this;
+	var _this = this;
 	
 	allCateTimer = setTimeout(function(){
 		var bottomHeight = document.documentElement.clientHeight - ev.clientY;
 		if(bottomHeight <= 250){
-			$($this).addClass('over').find('div.submenubox').addClass('submenuboxBottom').removeClass('disn');
+			$(_this).addClass('over').find('div.submenubox').addClass('submenuboxBottom').removeClass('disn');
 		}else{
-			$($this).addClass('over').find('div.submenubox').removeClass('submenuboxBottom').removeClass('disn');
+			$(_this).addClass('over').find('div.submenubox').removeClass('submenuboxBottom').removeClass('disn');
 		}
 		
 		//兼容IE6隐藏所有select 元素
@@ -20,19 +20,19 @@ $('div#nav > ul > li').hover(function(ev){
 			}
 		});
 		
-		if($($this).find("div.subcate").size()<= 0){
-			async_topMenu($this);
+		if($(_this).find("div.subcate").size()<= 0){
+			async_topMenu(_this);
 		}
 	}, delay);
 	
 },function(){
 	
-	var $this = this;
+	var _this = this;
 	if(allCateTimer){
 		clearTimeout(allCateTimer);
 	}
 	allCateTimer = setTimeout(function(){ 
-		$($this).removeClass('over').find('div.submenubox').addClass('disn');}, delay);
+		$(_this).removeClass('over').find('div.submenubox').addClass('disn');}, delay);
 	}
 	
 );
